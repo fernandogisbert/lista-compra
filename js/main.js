c9a8b70d58e9d1826025e9ebefebaed3bf9e9da2
LAPTOP-1U1SBC38\ferna <ferbert15@gmail.com>
2021-02-24 20:26:34 +0100

    mostrar, borrar y actualizar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 
 
-const url = "https://api.airtable.com/v0/appZjIeIIkjmA29vh/Productos?maxRecords=3&view=Grid%20view";
+const url = "https://api.airtable.com/v0/appZjIeIIkjmA29vh/Productos?&view=Grid%20view";
+const urlBorrar = "https://api.airtable.com/v0/appZjIeIIkjmA29vh/Productos?records[]=";
+const urlActualizar ="https://api.airtable.com/v0/appZjIeIIkjmA29vh/Productos"
 const authorization = 'Bearer keyswzmG8Q7KZoVp0';
 
 var app = new Vue({
     el: '#app',
     data: {
-
-        productos: []
+        productos: [],
+        productoActualizar: ''
     },
     mounted: function(){
         this.obtenerProductos();
@@ -31,6 +33,72 @@ var app = new Vue({
                     this.productos = json.records;
                 });
 
+        },
+        borrarProducto: function(id) {
+            // borramos del api
+            fetch(urlBorrar.concat(id),{
+                headers: {
+                    'Authorization': authorization
+                },
+                method: 'DELETE'
+            });
+            //borramos del local
+            this.productos = this.productos.filter(producto => {
+                return producto.id !== id
+            })
+        },
+        actualizarProductoEnAPI: function(id,nuevoTexto) {
+
+            fetch(urlActualizar, {
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization': authorization
+
+                },
+                method: 'PATCH',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                          "id": id,
+                          "fields": {
+                            "Nombre": nuevoTexto
+                          }
+                        }]
+                })
+            });
+        },
+        actualizarAdquiridoEnAPI: function(id,checked) {
+
+            fetch(urlActualizar, {
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization': authorization
+
+                },
+                method: 'PATCH',
+                body: JSON.stringify({
+                    "records": [
+                        {
+                          "id": id,
+                          "fields": {
+                            "Adquirido": checked
+                          }
+                        }]
+                })
+            });
+
+            //actualizamos en local
+            this.productos = this.productos.map((producto) => {
+
+                if(producto.id === id){
+                    let miProducto = producto;
+                    miProducto.fields.Adquirido = checked;
+                    return miProducto;
+                }else{
+                    return producto;
+                }
+
+            })
         }
     }
 
